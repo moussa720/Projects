7ff0cd140fb9e569968c92deb9d9d70665b3984c
moussa720 <moussa720@users.noreply.github.com>
2023-07-26 11:10:44 -0700

    last changes

--- lib/screens/api_client.dart
+++ lib/screens/api_client.dart
@@ -1,36 +1,48 @@
 import 'package:dio/dio.dart';
-class ApiClient{
-  final Dio dio=Dio();
-  ApiClient()
-  {
-    dio.options.baseUrl='http://127.0.0.1:8000/';
-  }
-  Future<String?>login(String username,String password)async{
-  var response=await dio.post('/login/',data: {
-    'username':username,
-    'password':password,
-  });
-  if (response.statusCode==200){
-    return response.data['token'];
-  }
-  else
-  {
-  return null;
-  }
-  }
-  Future<String?> register(String firstName, String lastName,String username,String email,String password) async {
-  var response = await dio.post('/register/', data: {
-    'First Name': firstName,
-    'Last Name': lastName,
-    'Username' :username,
-    'Email':email,
-    'Password':password,
-  });
 
-  if (response.statusCode == 200) {
-    return response.data['token'];
-  } else {
-    return null;
+class ApiClient {
+  final Dio dio = Dio();
+
+  ApiClient() {
+    dio.options.baseUrl = 'http://127.0.0.1:8000/';
   }
-}
+
+  Future<String?> login(String username, String password) async {
+    try {
+      var response = await dio.post('api/login/', data: {
+        'username': username,
+        'password': password,
+      });
+
+      if (response.statusCode == 200) {
+        return response.data['token'];
+      } else {
+        return null;
+      }
+    } catch (e) {
+      print('Error occurred while login: $e');
+      return null;
+    }
+  }
+
+  Future<String?> register(String firstName, String lastName, String username, String email, String password) async {
+    try {
+      var response = await dio.post('api/register/', data: {
+        'first_name': firstName,
+        'last_name': lastName,
+        'email': email,
+        'password': password,
+        'username': username,
+      });
+
+      if (response.statusCode == 200) {
+        return response.data['token'];
+      } else {
+        return null;
+      }
+    } catch (e) {
+      print('Error occurred while registration: $e');
+      return null;
+    }
+  }
 }
